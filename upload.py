5c11210dc1a9f15c6b0c4cc1dfbff4e20731aa32
Klemens David Morgenstern <klemens.morgenstern@gmx.net>
2019-02-01 22:23:28 +0800

    added rspec, minitest and testunit

--- upload.py
+++ upload.py
@@ -44,7 +44,8 @@ parser.add_argument("-l", "--file_list", nargs='+', help="Explicit file list, if
 parser.add_argument("-d", "--dir",   help="Directory to search for test reports, defaults to project root.")
 parser.add_argument("-t", "--token", help="Token to authenticate (not needed for public projects on appveyor, travis and circle-ci")
 parser.add_argument("-n", "--name", help="Custom defined name of the upload when commiting several builds with the same ci system")
-parser.add_argument("-f", "--framework", choices=["boost", "junit", "testng", "xunit", "cmocka", "unity", "criterion", "bandit", "catch", "cpputest", "cute", "cxxtest", "gtest", "qtest", "go"],
+parser.add_argument("-f", "--framework", choices=["boost", "junit", "testng", "xunit", "cmocka", "unity", "criterion", "bandit",
+                                                  "catch", "cpputest", "cute", "cxxtest", "gtest", "qtest", "go", "testunit", "rspec", "minitest"],
                                         help="The used unit test framework - if not provided the script will try to determine it")
 parser.add_argument("-r", "--root_dir", help="The root directory of the git-project, to be used for aligning paths properly. Default is the git-root.")
 parser.add_argument("-s", "--ci_system", help="Set the CI System manually. Should not be needed")
@@ -388,6 +389,8 @@ catch_test = []
 cxxtest = []
 qtest = []
 go_test = []
+testunit = []
+rspec = []
 
 if not args.file_list:
   for wk in os.walk(root_dir):
@@ -407,12 +410,6 @@ else:
     else:
       file_list.append(abs)
 
-
-def loadJson(tup): #returning nothing if not loadable
-  try:
-    return json.loads(tup)
-  except:  # includes JSONDecodeError
-    return None
 
 for abs_file in file_list:
   if match_file(abs_file):
@@ -455,6 +452,9 @@ for abs_file in file_list:
         criterion_test.append(content)
       if re.match(r'(<\?[^?]*\?>\s*)?<Catch\s+name=', content):
         catch_test.append(content)
+      if re.match(r'(<\?[^?]*\?>\s*)?<stream>\s*<ready-test-suite>', content):
+        testunit.append(content)
+
     elif re.match(r"\s*({|\[)", content): #Might be JSON, let's see if it fits go
       try:
         lines = content.splitlines();
@@ -462,6 +462,13 @@ for abs_file in file_list:
         if all(val in json_lines[0] for val in ["Time","Action","Package","Test"]): #assumption
           go_test = go_test + [json.loads(ln) for ln in lines]
         continue
+      except:
+        pass
+      try:
+        data = json.loads(content)
+        if "version" in data and "examples" in data and "summary" in data and "summary_line" in data and "profile" in data:
+          rspec.append(data);
+          continue
       except:
         pass
       #data = loadJson(content)
@@ -513,6 +520,10 @@ if not args.framework:
   elif len(go_test) > 0:
     framework = "go-test"
     print(bcolors.HEADER + "Go detected" + bcolors.ENDC)
+  elif len(testunit) > 0:
+    framework = "testunit"
+    print(bcolors.HEADER + "TestUnit detected" + bcolors.ENDC)
+
   else:
     print(bcolors.FAIL + "No framework selected and not detected." + bcolors.ENDC)
     exit(1)
@@ -560,6 +571,10 @@ elif (framework == "cpputest"):
   content_type = "text/xml"
   upload_content = "<root>" + "".join(xunit_test) + "</root>"
   if not run_name: run_name = "CppUTest"
+elif (framework == "minitest"):
+  content_type = "text/xml"
+  upload_content = "<root>" + "".join(xunit_test) + "</root>"
+  if not run_name: run_name = "CppUTest"
 elif (framework == "cute"):
   content_type = "text/xml"
   upload_content = "<root>" + "".join(xunit_test) + "</root>"
@@ -576,6 +591,14 @@ elif framework == "qtest":
   content_type = "text/xml"
   upload_content = "<root>" + "".join(qtest) + "</root>"
   if not run_name: run_name = "QTest"
+elif framework == "testunit":
+  content_type = "text/xml"
+  upload_content = "<root>" + "".join(testunit) + "</root>"
+  if not run_name: run_name = "TestUnit"
+elif framework == "rspec":
+  content_type = "application/json"
+  upload_content = json.dumps(rspec)
+  if not run_name: run_name = "RSpec";
 elif framework == "go-test":
   content_type = "application/json"
   upload_content = json.dumps({'files' : file_list, 'test_data': go_test})
